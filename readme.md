4c643be46ac3ab2f23969a80b7f2e8fbf194c681
alanzhang001 <alanzhang001@qq.com>
2018-04-20 21:07:25 +0800

    补充测试用例

--- readme.md
+++ readme.md
@@ -1,36 +1,76 @@
 # npm-example组件说明
 
+- 修正原生toFixed方法的精度问题
+- 支持四舍五入和直接截取
+
 ## 安装
 
 ```sh
-npm install  --- --save
+npm install num-toFixed --save
 ```
 
 ## 使用
 
-demo
-运行demo可执行命令(要求nodejs>=7.6):
+测试用例
 ```
 npm install
-npm run example
+npm test
 ```
 
 示例：
-
-
-js代码
-
-配置
-```javascript
-
 ```
-[更多demo](./example/index.html)
+// 通过script标签引入
+// var toFixed = window.toFixed;
 
+var toFixed = require('num-toFixed');
+
+// 精确度取四舍五入值
+toFixed(0.099999,3,true) //  '0.100'
+toFixed(0.999999,3,true) //  '1.000'
+toFixed(0.09,2,true)     //  '0.09'
+toFixed(0.09,1,true)     //  '0.1'
+toFixed(0.09,5,true)     //  '0.09000'
+toFixed(0.09,0,true)     //  '0'
+toFixed(0.99,3,true)     //  '0.990'
+toFixed(0.99,1,true)     //  '1.0'
+toFixed(.99,3,true)      //  '0.990'
+
+toFixed(1.099999,3,true)  // '1.100'
+toFixed(1.999999,3,true)  // '2.000'
+toFixed(1.09,2,true)      // '1.09'
+toFixed(1.09,1,true)      // '1.1'
+toFixed(1.09,5,true)      // '1.09000'
+toFixed(9.09,0,true)      // '9'
+toFixed(9.99,3,true)      // '9.990'
+toFixed(9.99,1,true)      // '10.0'
+toFixed(199.9999,3,true)  // '200.000'
+
+// 根据精确度直接截取小数点
+toFixed(0.099999,3,false) // '0.099'
+toFixed(0.09,2,false)     // '0.09'
+toFixed(0.09,1,false)     // '0.0'
+toFixed(0.09,5,false)     // '0.09000'
+toFixed(0.09,0,false)     // '0'
+toFixed(0.99,3,false)     // '0.990'
+toFixed(.99,3,false)      // '0.990'
+```
+
+### API
+
+#### toFixed(num,digit,isRound)
+
+**说明：** 修正原生toFixed方法的精度问题，支持四舍五入和直接截取
+
+**num:** `String|Number`, 需要取精度的数字或则可转换为数字的字符串
+
+**digit:** `Number` 保留的小数点位数，可取范围为(0,20],末尾不足位数时补0
+
+**isRound**：`Boolean` 保留小数点时是否进行四舍五入，默认为false，即直接截取，不进行四舍五入
+
+**return** `String`
 
 ## 版本记录
 
-#### v1.0.0 2017-12-27
+#### v1.0.0 2018-04-20
 
 - 组件发布
-
-DEMO
--- test/unittest/lib.test.js
+++ test/unittest/lib.test.js
@@ -1,28 +1,251 @@
 /* global chai describe it expect  FileUploadLIB*/
 var expect = chai.expect;
 
-describe('XXX方法测试', function() {
-    this.timeout(5000);
+describe('toFixed方法测试-直接截取', function() {
 
-
-    it('XXXFn：xxxx获取xx成功', function() {
-        expect(1).to.equal(1);
+    it('正浮点数[Number]-小于1-toFixed', function() {
+        expect(toFixed(0.099999,3,false)).to.equal('0.099');
+        expect(toFixed(0.09,2,false)).to.equal('0.09');
+        expect(toFixed(0.09,1,false)).to.equal('0.0');
+        expect(toFixed(0.09,5,false)).to.equal('0.09000');
+        expect(toFixed(0.09,0,false)).to.equal('0');
+        expect(toFixed(0.99,3,false)).to.equal('0.990');
+        expect(toFixed(.99,3,false)).to.equal('0.990');
     });
 
-    it('XXXFn：xxxx设置xx成功', function() {
-        expect(2).to.equal(2);
+    it('正浮点数[Number]-大于1-toFixed', function() {
+        expect(toFixed(1.099999,3,false)).to.equal('1.099');
+        expect(toFixed(1.09,2,false)).to.equal('1.09');
+        expect(toFixed(1.09,1,false)).to.equal('1.0');
+        expect(toFixed(1.09,5,false)).to.equal('1.09000');
+        expect(toFixed(1.09,0,false)).to.equal('1');
+        expect(toFixed(1.01,2,false)).to.equal('1.01');
+        expect(toFixed(1.01,2,false)).to.equal('1.01');
+        expect(toFixed(1.,2,false)).to.equal('1.00');
     });
+
+    it('负浮点数[Number]-大于-1-toFixed', function() {
+        expect(toFixed(-0.09,2,false)).to.equal('-0.09');
+        expect(toFixed(-0.09,1,false)).to.equal('-0.0');
+        expect(toFixed(-0.09,5,false)).to.equal('-0.09000');
+        expect(toFixed(-0.09,0,false)).to.equal('-0');
+        expect(toFixed(-.09,1,false)).to.equal('-0.0');
+        expect(toFixed(-0.01,2,false)).to.equal('-0.01');
+        expect(toFixed(-.01,2,false)).to.equal('-0.01');
+    });
+
+    it('负浮点数[Number]-小于-1-toFixed', function() {
+        expect(toFixed(-1.09,2,false)).to.equal('-1.09');
+        expect(toFixed(-1.09,1,false)).to.equal('-1.0');
+        expect(toFixed(-1.09,5,false)).to.equal('-1.09000');
+        expect(toFixed(-1.09,0,false)).to.equal('-1');
+        expect(toFixed(-1.09,1,false)).to.equal('-1.0');
+        expect(toFixed(-1.01,2,false)).to.equal('-1.01');
+        expect(toFixed(-1.01,2,false)).to.equal('-1.01');
+    });
+
+    it('正整数[Number]-toFixed', function() {
+        expect(toFixed(1,3,false)).to.equal('1.000');
+        expect(toFixed(19,2,false)).to.equal('19.00');
+        expect(toFixed(399,1,false)).to.equal('399.0');
+        expect(toFixed(22,5,false)).to.equal('22.00000');
+        expect(toFixed(3,0,false)).to.equal('3');
+    });
+
+    it('负整数[Number]-toFixed', function() {
+        expect(toFixed(-1,3,false)).to.equal('-1.000');
+        expect(toFixed(-19,2,false)).to.equal('-19.00');
+        expect(toFixed(-399,1,false)).to.equal('-399.0');
+        expect(toFixed(-22,5,false)).to.equal('-22.00000');
+        expect(toFixed(-3,0,false)).to.equal('-3');
+    });
+
+    it('正浮点数[String]-小于1-toFixed', function() {
+        expect(toFixed('0.099999',3,false)).to.equal('0.099');
+        expect(toFixed('0.09',2,false)).to.equal('0.09');
+        expect(toFixed('0.09',1,false)).to.equal('0.0');
+        expect(toFixed('0.09',5,false)).to.equal('0.09000');
+        expect(toFixed('0.09',0,false)).to.equal('0');
+        expect(toFixed('0.99',3,false)).to.equal('0.990');
+        expect(toFixed('.99',3,false)).to.equal('0.990');
+    });
+
+    it('正浮点数[String]-大于1-toFixed', function() {
+        expect(toFixed('1.099999',3,false)).to.equal('1.099');
+        expect(toFixed('1.09',2,false)).to.equal('1.09');
+        expect(toFixed('1.09',1,false)).to.equal('1.0');
+        expect(toFixed('1.09',5,false)).to.equal('1.09000');
+        expect(toFixed('1.09',0,false)).to.equal('1');
+        expect(toFixed('1.01',2,false)).to.equal('1.01');
+        expect(toFixed('1.01',2,false)).to.equal('1.01');
+        expect(toFixed('1.',2,false)).to.equal('1.00');
+    });
+
+    it('负浮点数[String]-大于-1-toFixed', function() {
+        expect(toFixed('-0.09',2,false)).to.equal('-0.09');
+        expect(toFixed('-0.09',1,false)).to.equal('-0.0');
+        expect(toFixed('-0.09',5,false)).to.equal('-0.09000');
+        expect(toFixed('-0.09',0,false)).to.equal('-0');
+        expect(toFixed('-.09',1,false)).to.equal('-0.0');
+        expect(toFixed('-0.01',2,false)).to.equal('-0.01');
+        expect(toFixed('-.01',2,false)).to.equal('-0.01');
+    });
+
+    it('负浮点数[String]-小于-1-toFixed', function() {
+        expect(toFixed('-1.09',2,false)).to.equal('-1.09');
+        expect(toFixed('-1.09',1,false)).to.equal('-1.0');
+        expect(toFixed('-1.09',5,false)).to.equal('-1.09000');
+        expect(toFixed('-1.09',0,false)).to.equal('-1');
+        expect(toFixed('-1.09',1,false)).to.equal('-1.0');
+        expect(toFixed('-1.01',2,false)).to.equal('-1.01');
+        expect(toFixed('-1.01',2,false)).to.equal('-1.01');
+    });
+
+    it('正整数[String]-toFixed', function() {
+        expect(toFixed('1',3,false)).to.equal('1.000');
+        expect(toFixed('19',2,false)).to.equal('19.00');
+        expect(toFixed('399',1,false)).to.equal('399.0');
+        expect(toFixed('22',5,false)).to.equal('22.00000');
+        expect(toFixed('3',0,false)).to.equal('3');
+    });
+
+    it('负整数[String]-toFixed', function() {
+        expect(toFixed('-1',3,false)).to.equal('-1.000');
+        expect(toFixed('-19',2,false)).to.equal('-19.00');
+        expect(toFixed('-399',1,false)).to.equal('-399.0');
+        expect(toFixed('-22',5,false)).to.equal('-22.00000');
+        expect(toFixed('-3',0,false)).to.equal('-3');
+    });
+
 });
 
-describe('XXX2方法测试', function() {
-    this.timeout(5000);
+describe('toFixed方法测试-四舍五入', function() {
 
-    it('XXXFn：xxxx获取xx成功', function() {
-        expect(1).to.equal(1);
+    it('正浮点数[Number]-小于1-toFixed', function() {
+        expect(toFixed(0.099999,3,true)).to.equal('0.100');
+        expect(toFixed(0.999999,3,true)).to.equal('1.000');
+        expect(toFixed(0.09,2,true)).to.equal('0.09');
+        expect(toFixed(0.09,1,true)).to.equal('0.1');
+        expect(toFixed(0.09,5,true)).to.equal('0.09000');
+        expect(toFixed(0.09,0,true)).to.equal('0');
+        expect(toFixed(0.99,3,true)).to.equal('0.990');
+        expect(toFixed(0.99,1,true)).to.equal('1.0');
+        expect(toFixed(.99,3,true)).to.equal('0.990');
     });
 
-    it('XXXFn2：xxxx设置xx成功', function() {
-        expect(2).to.equal(2);
+    it('正浮点数[Number]-大于1-toFixed', function() {
+        expect(toFixed(1.099999,3,true)).to.equal('1.100');
+        expect(toFixed(1.999999,3,true)).to.equal('2.000');
+        expect(toFixed(1.09,2,true)).to.equal('1.09');
+        expect(toFixed(1.09,1,true)).to.equal('1.1');
+        expect(toFixed(1.09,5,true)).to.equal('1.09000');
+        expect(toFixed(9.09,0,true)).to.equal('9');
+        expect(toFixed(9.99,3,true)).to.equal('9.990');
+        expect(toFixed(9.99,1,true)).to.equal('10.0');
+        expect(toFixed(199.9999,3,true)).to.equal('200.000');
+    });
+
+    it('负浮点数[Number]-大于-1-toFixed', function() {
+        expect(toFixed(-0.099999,3,true)).to.equal('-0.100');
+        expect(toFixed(-0.999999,3,true)).to.equal('-1.000');
+        expect(toFixed(-0.09,2,true)).to.equal('-0.09');
+        expect(toFixed(-0.09,1,true)).to.equal('-0.1');
+        expect(toFixed(-0.09,5,true)).to.equal('-0.09000');
+        expect(toFixed(-0.09,0,true)).to.equal('-0');
+        expect(toFixed(-0.99,3,true)).to.equal('-0.990');
+        expect(toFixed(-0.99,1,true)).to.equal('-1.0');
+        expect(toFixed(-.99,3,true)).to.equal('-0.990');
+    });
+
+    it('负浮点数[Number]-小于-1-toFixed', function() {
+        expect(toFixed(-1.099999,3,true)).to.equal('-1.100');
+        expect(toFixed(-1.999999,3,true)).to.equal('-2.000');
+        expect(toFixed(-1.09,2,true)).to.equal('-1.09');
+        expect(toFixed(-1.09,1,true)).to.equal('-1.1');
+        expect(toFixed(-1.09,5,true)).to.equal('-1.09000');
+        expect(toFixed(-9.09,0,true)).to.equal('-9');
+        expect(toFixed(-9.99,3,true)).to.equal('-9.990');
+        expect(toFixed(-9.99,1,true)).to.equal('-10.0');
+        expect(toFixed(-199.9999,3,true)).to.equal('-200.000');
+    });
+
+    it('正整数[Number]-toFixed', function() {
+        expect(toFixed(1,3,true)).to.equal('1.000');
+        expect(toFixed(19,2,true)).to.equal('19.00');
+        expect(toFixed(399,1,true)).to.equal('399.0');
+        expect(toFixed(22,5,true)).to.equal('22.00000');
+        expect(toFixed(3,0,true)).to.equal('3');
+    });
+
+    it('负整数[Number]-toFixed', function() {
+        expect(toFixed(-1,3,true)).to.equal('-1.000');
+        expect(toFixed(-19,2,true)).to.equal('-19.00');
+        expect(toFixed(-399,1,true)).to.equal('-399.0');
+        expect(toFixed(-22,5,true)).to.equal('-22.00000');
+        expect(toFixed(-3,0,true)).to.equal('-3');
+    });
+
+        it('正浮点数[String]-小于1-toFixed', function() {
+        expect(toFixed('0.099999',3,true)).to.equal('0.100');
+        expect(toFixed('0.999999',3,true)).to.equal('1.000');
+        expect(toFixed('0.09',2,true)).to.equal('0.09');
+        expect(toFixed('0.09',1,true)).to.equal('0.1');
+        expect(toFixed('0.09',5,true)).to.equal('0.09000');
+        expect(toFixed('0.09',0,true)).to.equal('0');
+        expect(toFixed('0.99',3,true)).to.equal('0.990');
+        expect(toFixed('0.99',1,true)).to.equal('1.0');
+        expect(toFixed('.99',3,true)).to.equal('0.990');
+    });
+
+    it('正浮点数[String]-大于1-toFixed', function() {
+        expect(toFixed('1.099999',3,true)).to.equal('1.100');
+        expect(toFixed('1.999999',3,true)).to.equal('2.000');
+        expect(toFixed('1.09',2,true)).to.equal('1.09');
+        expect(toFixed('1.09',1,true)).to.equal('1.1');
+        expect(toFixed('1.09',5,true)).to.equal('1.09000');
+        expect(toFixed('9.09',0,true)).to.equal('9');
+        expect(toFixed('9.99',3,true)).to.equal('9.990');
+        expect(toFixed('9.99',1,true)).to.equal('10.0');
+        expect(toFixed('199.9999',3,true)).to.equal('200.000');
+    });
+
+    it('负浮点数[String]-大于-1-toFixed', function() {
+        expect(toFixed('-0.099999',3,true)).to.equal('-0.100');
+        expect(toFixed('-0.999999',3,true)).to.equal('-1.000');
+        expect(toFixed('-0.09',2,true)).to.equal('-0.09');
+        expect(toFixed('-0.09',1,true)).to.equal('-0.1');
+        expect(toFixed('-0.09',5,true)).to.equal('-0.09000');
+        expect(toFixed('-0.09',0,true)).to.equal('-0');
+        expect(toFixed('-0.99',3,true)).to.equal('-0.990');
+        expect(toFixed('-0.99',1,true)).to.equal('-1.0');
+        expect(toFixed('-.99',3,true)).to.equal('-0.990');
+    });
+
+    it('负浮点数[String]-小于-1-toFixed', function() {
+        expect(toFixed('-1.099999',3,true)).to.equal('-1.100');
+        expect(toFixed('-1.999999',3,true)).to.equal('-2.000');
+        expect(toFixed('-1.09',2,true)).to.equal('-1.09');
+        expect(toFixed('-1.09',1,true)).to.equal('-1.1');
+        expect(toFixed('-1.09',5,true)).to.equal('-1.09000');
+        expect(toFixed('-9.09',0,true)).to.equal('-9');
+        expect(toFixed('-9.99',3,true)).to.equal('-9.990');
+        expect(toFixed('-9.99',1,true)).to.equal('-10.0');
+        expect(toFixed('-199.9999',3,true)).to.equal('-200.000');
+    });
+
+    it('正整数[String]-toFixed', function() {
+        expect(toFixed('1',3,true)).to.equal('1.000');
+        expect(toFixed('19',2,true)).to.equal('19.00');
+        expect(toFixed('399',1,true)).to.equal('399.0');
+        expect(toFixed('22',5,true)).to.equal('22.00000');
+        expect(toFixed('3',0,true)).to.equal('3');
+    });
+
+    it('负整数[String]-toFixed', function() {
+        expect(toFixed('-1',3,true)).to.equal('-1.000');
+        expect(toFixed('-19',2,true)).to.equal('-19.00');
+        expect(toFixed('-399',1,true)).to.equal('-399.0');
+        expect(toFixed('-22',5,true)).to.equal('-22.00000');
+        expect(toFixed('-3',0,true)).to.equal('-3');
     });
 });
-
